ca36fded34db2a3f3b9a6ff7731c8c7d7508e5de
Gabriel Siebra <gabrielsiebra2014@gmail.com>
2026-03-04 16:42:42 -0300

    starting header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,14 @@
 const Header = () => {
     return (
         <>
+        <header className="header-container">
+            <div className="logo">
+                <img src="https://raw.githubusercontent.com/digitalcollegebr/projeto-digital-store/154c105f9b1a127fb30ca797a224a9a301439ed5/assets/logo-header.svg" />
+            </div>
+            <div className="searchBox">
+                <input type="text" placeholder="Pesquisar produtos..."/>
+            </div>
+        </header>
         </>
     );
 };
--- src/components/ProductFile.jsx
+++
@@ -1,8 +0,0 @@
-const ProductFile = () => {
-    return (
-        <>
-        </>
-    );
-};
-
-export default ProductFile;
