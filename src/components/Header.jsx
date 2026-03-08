0fe5486556c690ed09ee8c124d9e1850b1895aa1
Gabriel Siebra <gabrielsiebra2014@gmail.com>
2026-03-08 20:40:17 -0300

    header ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,38 @@
+import logoImg from '../assets/logo-header.svg'
+import 'primeicons/primeicons.css';
+
 const Header = () => {
     return (
         <>
         <header className="header-container">
-            <div className="logo">
-                <img src="https://raw.githubusercontent.com/digitalcollegebr/projeto-digital-store/154c105f9b1a127fb30ca797a224a9a301439ed5/assets/logo-header.svg" />
+            <div className='header-top'>
+                <div className="logo">
+                    <img src={logoImg}/>
             </div>
             <div className="searchBox">
                 <input type="text" placeholder="Pesquisar produtos..."/>
+                <i className="pi pi-search search-icon"></i>
             </div>
-        </header>
+
+                <div className="header-actions">
+                    <a href="/cadastro" className="link-cadastre">Cadastre-se</a>
+                    <button className="btn-entrar">Entrar</button>
+                    <div className="cart-container">
+                        <i className='pi pi-cart-plus cart-icon'></i>
+                        <span className="cart-badge"></span>
+                    </div>
+                </div>
+            </div>
+
+        <nav className="header-nav">
+                <ul>
+                    <li><a href="/" className="active">Home</a></li>
+                    <li><a href="/produtos">Produtos</a></li>
+                    <li><a href="/categorias">Categorias</a></li>
+                    <li><a href="/pedidos">Meus Pedidos</a></li>
+                </ul>
+        </nav>
+    </header>
         </>
     );
 };
