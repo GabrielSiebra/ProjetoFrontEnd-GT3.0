f3c5c5c019f5a9fede530be281feea8220fe8503
Gabriel Siebra <gabrielsiebra2014@gmail.com>
2026-03-09 12:27:42 -0300

    Product Card ok

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,21 @@
-const ProductCard = () => {
+const ProductCard = ({ image, category, name, price, discountPrice }) => {
     return (
         <>
+        <div className="product-card">
+            <div className="product-image">
+                <img src={image} alt={name} />
+            </div>
+            
+            <div className="product-info">
+                <span className="product-category">{category}</span>
+                <h3 className="product-name">{name}</h3>
+                
+                <div className="product-prices">
+                <span className="price-old">R$ {price}</span>
+                <span className="price-new">R$ {discountPrice}</span>
+                </div>
+            </div>
+        </div>
         </>
     );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,37 @@
+import ProductCard from "../components/ProductCard";
+import sneakerImg from '../assets/sneaker-1.svg'
+
 const Home = () => {
+    const trendingProducts = [
+  
+  { id: 1, name: "Tênis K-Swiss V8 - Masculino", category: "Tênis", price: "200,00", discountPrice: "100,00", image: sneakerImg },
+  { id: 2, name: "Tênis K-Swiss V8 - Masculino", category: "Tênis", price: "200,00", discountPrice: "100,00", image: sneakerImg },
+  { id: 3, name: "Tênis K-Swiss V8 - Masculino", category: "Tênis", price: "200,00", discountPrice: "100,00", image: sneakerImg },
+  { id: 4, name: "Tênis K-Swiss V8 - Masculino", category: "Tênis", price: "200,00", discountPrice: "100,00", image: sneakerImg },
+  { id: 5, name: "Tênis K-Swiss V8 - Masculino", category: "Tênis", price: "200,00", discountPrice: "100,00", image: sneakerImg },
+  { id: 6, name: "Tênis K-Swiss V8 - Masculino", category: "Tênis", price: "200,00", discountPrice: "100,00", image: sneakerImg },
+  { id: 7, name: "Tênis K-Swiss V8 - Masculino", category: "Tênis", price: "200,00", discountPrice: "100,00", image: sneakerImg },
+  { id: 8, name: "Tênis K-Swiss V8 - Masculino", category: "Tênis", price: "200,00", discountPrice: "100,00", image: sneakerImg }
+  ];
     return (
         <>
+        <div className="home-container">
+            <section className="trending-products">
+                <h2>Produtos em alta</h2>               
+                <div className="product-grid">
+                {trendingProducts.map((product) => (
+                    <ProductCard
+                    key={product.id}
+                    image={product.image}
+                    category={product.category}
+                    name={product.name}
+                    price={product.price}
+                    discountPrice={product.discountPrice}
+                    />
+                ))}
+                </div>
+            </section>
+        </div>
         </>
     );
 };
